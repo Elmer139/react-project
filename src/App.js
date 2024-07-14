89337a23a6aa453d73c5320293c4a56904614769
Shawn Conrad <mrshawnconrad@gmail.com>
2024-07-14 18:07:07 -0400

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  const topic = "React";
+  const age = 39;
+  const likes = ["React", "JavaScript", "TypeScript"];
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div id="someId">
+      {/* This is a comment */}
+      <h1 id="headerId">{topic} is Awesome!</h1>
+      <h2>{age}</h2>
+      <h3>{likes}</h3>
+      <h4>{String(true)}</h4>
+      {true ? <p>Yup it's true</p> : <p>Nope it's false</p>}
     </div>
   );
 }
