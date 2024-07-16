d8fefe5e258bf5423dbf4e1bfe77e035122742d3
Shawn Conrad <mrshawnconrad@gmail.com>
2024-07-16 07:56:10 -0400

    in progress

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const topic = "React";
-  const age = 39;
-  const likes = ["React", "JavaScript", "TypeScript"];
-  return (
-    <div id="someId">
-      {/* This is a comment */}
-      <h1 id="headerId">{topic} is Awesome!</h1>
-      <h2>{age}</h2>
-      <h3>{likes}</h3>
-      <h4>{String(true)}</h4>
-      {true ? <p>Yup it's true</p> : <p>Nope it's false</p>}
-    </div>
-  );
+	const pets = [
+		{
+			name: "Purrsloud",
+			species: "Cat",
+			favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+			birthYear: 2016,
+			photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+		},
+		{
+			name: "Barksalot",
+			species: "Dog",
+			birthYear: 2008,
+			photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg",
+		},
+		{
+			name: "Meowsalot",
+			species: "Cat",
+			favFoods: ["tuna", "catnip", "celery"],
+			birthYear: 2012,
+			photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg",
+		},
+	];
+	return (
+		<div>
+			<h1>Random Pets</h1>
+			<ul>
+				{pets.map((pet, index) => {
+					return (
+						<li key={index}>
+							<h2 key={pet}>{pet.name}</h2>
+							<h3>{pet.species}</h3>
+							<img src={pet.photo} alt={pet.name} />
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
-
 export default App;
